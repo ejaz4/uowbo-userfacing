6f66e647438432663a99df602b0d9da00b9764a1
ejaz4 <ejazali4@protonmail.ch>
2024-11-02 20:11:36 +0000

    💄 Skeleton loader

--- src/app/verify/[handover]/page.tsx
+++ src/app/verify/[handover]/page.tsx
@@ -1,15 +1,15 @@
 "use client";
-import { Handover } from "@prisma/client";
 import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { DiscordDisplay } from "@/app/_components/profileDisplay/discord";
+import { Skeleton } from "@/app/_components/skeleton/skeleton";
+import { Card } from "@/app/_components/card/card";
+// import { Skeleton } from "@chakra-ui/skeleton";
 
-export const HandoverPage = ({
-  params,
-}: {
-  params: {
-    handover: string;
-  };
-}) => {
+export const HandoverPage = () => {
+  const params = useParams<{ handover: string }>();
   const handoverId = params.handover;
+  const [loading, setLoading] = useState(true);
   const [username, setUsername] = useState("");
   const [id, setId] = useState("");
   const [avatar, setAvatar] = useState("");
@@ -41,15 +41,41 @@ export const HandoverPage = ({
       setUsername(data.DiscordUser.username);
       setAvatar(data.DiscordUser.avatar);
       setId(data.DiscordUser.discordId);
+      setLoading(false);
     })();
   }, []);
 
   return (
-    <>
-      <p>Name: {username}</p>
-      <p>ID: {id}</p>
-      <img src={avatar} alt="" />
-    </>
+    <Card>
+      {!loading && (
+        <>
+          <h2>Let&apos;s link your account, {username}</h2>
+          <p>
+            To continue linking your Discord server profile to your University
+            of Westminster account, click Link.
+          </p>
+          <DiscordDisplay
+            username={username}
+            avatarUrl={avatar}
+            verified={false}
+          />
+        </>
+      )}
+
+      {loading && (
+        <>
+          <Skeleton width={"40%"} height={16}></Skeleton>
+          <div style={{ display: "flex", flexDirection: "column", gap: 5 }}>
+            <Skeleton width={"100%"} height={16} count={2}></Skeleton>
+          </div>
+          <DiscordDisplay
+            username={username}
+            avatarUrl={avatar}
+            verified={false}
+          />
+        </>
+      )}
+    </Card>
   );
 };
 
