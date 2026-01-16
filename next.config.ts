1b4c72ba67d87a0b63100557de0f5ffad251eab3
ejaz4 <ejazali4@protonmail.ch>
2026-01-16 04:18:58 +0000

    Remove ESLint dependency from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
